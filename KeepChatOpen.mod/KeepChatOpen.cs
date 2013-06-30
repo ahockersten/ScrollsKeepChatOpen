adb0bb63783513ea644a87df40494819bb21808c
Anders Höckersten <anders@hockersten.se>
2013-06-30 20:31:51 +0200

    Add finished version
    Well, that was easy.

--- KeepChatOpen.mod/KeepChatOpen.cs
+++ KeepChatOpen.mod/KeepChatOpen.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reflection;
-using System.Text.RegularExpressions;
 
 using ScrollsModLoader.Interfaces;
 using UnityEngine;
@@ -17,6 +17,7 @@ namespace KeepChatOpen.mod {
         public KeepChatOpen() {
         }
 
+
         public static string GetName() {
             return "KeepChatOpen";
         }
@@ -26,12 +27,32 @@ namespace KeepChatOpen.mod {
         }
 
         public static MethodDefinition[] GetHooks(TypeDefinitionCollection scrollsTypes, int version) {
-            return new MethodDefinition[] { };
+            try {
+                return new MethodDefinition[] {
+                    scrollsTypes["ChatUI"].Methods.GetMethod("Show", new Type[]{typeof(bool)})
+                };
+            }
+            catch {
+                Console.WriteLine("KeepChatOpen failed to connect to methods used.");
+                return new MethodDefinition[] { };
+            }
         }
 
 
         public override bool BeforeInvoke(InvocationInfo info, out object returnValue) {
             returnValue = null;
+            if (info.target is ChatUI && info.targetMethod.Equals("Show")) {
+                foreach (StackFrame frame in info.stackTrace.GetFrames()) {
+                    // Disallow all except for:
+                    // ChatUI.OnGUI() - this gets called when the user clicks the open/close button
+                    // Lobby.Start() - this gets called when the user moves to the "arena" tab
+                    if ((frame.GetMethod().ReflectedType.Equals(typeof(ChatUI)) && frame.GetMethod().Name.Contains("OnGUI")) ||
+                        (frame.GetMethod().ReflectedType.Equals(typeof(Lobby)) && frame.GetMethod().Name.Contains("Start"))) {
+                        return false;
+                    }
+                }
+                return true;
+            }
             return false;
         }
 
